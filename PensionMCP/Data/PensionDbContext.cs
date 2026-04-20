4ebc698472ba22c31e0c346b4089d7f5f7c1fd99
Eoin Prout <eoinprout@gmail.com>
2026-04-20 20:25:55 +0100

    Spike: SQLIte prove of concept issue #3 Add DbContext with seed data Add PensionDbContext tests using in-memory SQLite

---
+++ PensionMCP/Data/PensionDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PensionMCP.Models;
+
+namespace PensionMCP.Data
+{
+    public class PensionDbContext : DbContext
+    {
+        public PensionDbContext(DbContextOptions<PensionDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Client> Clients => Set<Client>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //Seed data to help with demo and testing.
+            modelBuilder.Entity<Client>().HasData(
+                new Client { Id = 1, Name = "James Kirk", DateOfBirth = new DateOnly(1974, 12, 12) },
+                new Client { Id = 2, Name = "Nyota Uhura", DateOfBirth = new DateOnly(1982, 7, 25) },
+                new Client { Id = 3, Name = "Leonard McCoy", DateOfBirth = new DateOnly(1990, 11, 1) }
+            );
+        }
+    }
+}
---
+++ PensionMCP/Models/Client.cs
@@ -0,0 +1,9 @@
+﻿namespace PensionMCP.Models
+{
+    public class Client
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public DateOnly DateOfBirth { get; set; }
+    }
+}
