51ff7ee43969f09056614b1058f499e21159f208
Eoin Prout <eoinprout@gmail.com>
2026-04-21 18:12:26 +0100

    Task: Installer mcpb (Model Context Protocol Bundle) issue #4 Added a basic mcpb manifest file which is enough to use the mcpb CLI to create the mcpb installer file. Modified publish.bat to make the mcpb file and add it to the publish folder.

--- publish.bat
+++ publish.bat
@@ -1,2 +1,17 @@
+echo on
 :: Publish everything as a single a self contained file
 dotnet publish .\PensionMCP\PensionMCP.csproj -c Release -r win-x64 --self-contained -p:PublishSingleFile=true -p:IncludeNativeLibrariesForSelfExtract=true -o ./publish
+
+:: create the mcpb installer file
+if exist .\mcpb\server\PensionMCP.exe del /f /q .\mcpb\server\PensionMCP.exe
+copy /y .\publish\PensionMCP.exe .\mcpb\server\PensionMCP.exe
+
+pushd .\mcpb
+cmd /c mcpb pack
+popd
+
+if exist .\mcpb\PensionAssistant.mcpb del /f /q .\mcpb\PensionAssistant.mcpb
+ren .\mcpb\mcpb.mcpb PensionAssistant.mcpb
+copy /y .\mcpb\PensionAssistant.mcpb .\publish\PensionAssistant.mcpb
+echo Done.
+
