6e00b6a9f39b4eebffd893baa6098a0f5875dbe1
Eoin Prout <eoinprout@gmail.com>
2026-05-15 09:31:51 +0100

    fix linting error, trailing new line

--- PensionMCP/Engine/AgeCalculator.cs
+++ PensionMCP/Engine/AgeCalculator.cs
@@ -43,8 +43,7 @@
         {
             var retirementDate = GetDateTurnsAge(dateOfBirth, retirementAge);
             var months = ((retirementDate.Year - asOf.Year) * 12) + retirementDate.Month - asOf.Month;
-            //if (retirementDate.Day < asOf.Day)
-            //    months--;
+
             return Math.Max(months, 0);
         }
     }
